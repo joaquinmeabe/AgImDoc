1d412be5c735d1e2e71b778cfd9e00adb47e58bd
joaquin <joaquinmeabe@gmail.com>
2024-09-29 16:02:46 -0300

    arreglos en js css y html icono hamburguesa apertura y cierre

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,19 @@
-// Seleccionamos el ícono del menú y la lista de navegación
+/// Seleccionamos el ícono del menú y la lista de navegación
 const menuIcon = document.getElementById('menu-icon');
 const navList = document.getElementById('nav-list');
+const icon = document.getElementById('icon');
 
-// Función para alternar la visibilidad del menú
-menuIcon.addEventListener('click', () => {
-  navList.classList.toggle('show');
+// Añadimos un evento de clic al ícono del menú
+menuIcon.addEventListener('click', function () {
+    // Alternar la visibilidad del menú
+    navList.classList.toggle('show-nav');
+    
+    // Cambiar el ícono de hamburguesa a "X" y viceversa
+    if (icon.classList.contains('fa-bars')) {
+        icon.classList.remove('fa-bars');
+        icon.classList.add('fa-xmark');
+    } else {
+        icon.classList.remove('fa-xmark');
+        icon.classList.add('fa-bars');
+    }
 });
